c0c33adc926d8988723d751ac2f1bce81f01e406
swordmanofdmist <khadjiristov@gmail.com>
2026-04-17 23:49:44 +0200

    Hash service commit

---
+++ Services/Hashing.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HomeAssignment.Services
+{
+    public static class Hashing
+    {
+        public static string ComputeMD5(ReadOnlySpan<byte> data)
+        {
+            byte[] hash = MD5.HashData(data);
+            return Convert.ToHexString(hash);
+        }
+
+        public static async Task<string> ComputeFileSHA256Async(string filePath, CancellationToken ct = default)
+        {
+            using var hasher = IncrementalHash.CreateHash(HashAlgorithmName.SHA256);
+            await using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 81920, useAsync: true);
+            byte[] buffer = new byte[81920];
+            int bytesRead;
+            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, ct)) > 0)
+            {
+                hasher.AppendData(buffer, 0, bytesRead);
+            }
+            return Convert.ToHexString(hasher.GetHashAndReset());
+        }
+    }
+}
